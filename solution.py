544262de43c7cd2f4561b2a17b17775137cadcb8
hashar7 <86832867+hashar7@users.noreply.github.com>
2023-04-13 02:43:55 +0300

    Attempt 2

--- solution.py
+++ solution.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import anderson_ksamp
 
 chat_id = 458704720 # Ваш chat ID, не меняйте название переменной
 
 
 def solution(x: np.array, y: np.array) -> bool:
-    return ks_2samp(x, y, alternative="two-sided").pvalue >= 0.07
+    return anderson_ksamp([x, y]).pvalue < 0.07 
